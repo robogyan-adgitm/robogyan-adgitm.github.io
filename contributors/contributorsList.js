765585115feeaa18a4d57733fb0a71559a14a9da
Li0 <manojmarmat762000@gmail.com>
2022-10-07 23:23:52 +0530

    Fix: FAQ page and organise project structure
    Signed-off-by: Li0 <manojmarmat762000@gmail.com>

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1,7 +1,7 @@
 contributors = [
-    {
-      id: 1,
-      fullname: "Ashish",
-      username: "https://github.com/ashishthehulk",
-    },
-]
+  {
+    id: 1,
+    fullname: "Ashish",
+    username: "https://github.com/ashishthehulk",
+  },
+];
