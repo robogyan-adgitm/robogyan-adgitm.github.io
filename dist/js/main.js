8290b49c3f521a31b2ff628d127e1bc841e9b13b
anuragasawa20 <img_2021012@iiitm.ac.in>
2022-10-07 09:35:33 +0530

    FAQ_section_with Accordian

--- dist/js/main.js
+++ dist/js/main.js
@@ -5,3 +5,19 @@ hambtn.addEventListener("click", () => {
   navbar.classList.toggle("active");
   hambtn.classList.toggle("active");
 });
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
