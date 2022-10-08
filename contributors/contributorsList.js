42d2cf273ffcff88ae35a7a4285b20d99bdc16ae
yash-baroda <72942670+yash-baroda@users.noreply.github.com>
2022-10-08 20:34:20 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -3,5 +3,9 @@ contributors = [
     id: 1,
     fullname: "Ashish",
     username: "https://github.com/ashishthehulk",
+  }, {
+    id: 2,
+    fullname: "yash",
+    username: "https://github.com/yash-baroda",
   },
 ];
