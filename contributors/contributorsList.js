ed2f78f59da22a5d4d636064a3a1b777b500613a
Aksha mishra <84321203+aksha123-git@users.noreply.github.com>
2022-10-16 23:56:15 +0530

    Add Content to About Us Page

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -4,4 +4,10 @@ contributors = [
     fullname: "Ashish",
     username: "https://github.com/ashishthehulk",
   },
+  
+  {
+    id: 3,
+    fullname: "aksha mishra",
+    username: "https://github.com/aksha123-git",
+  },
 ];
